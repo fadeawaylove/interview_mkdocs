4c0f810995d81ce794f37dc03a466192ab1842d5
邓润庭 <dengrt@akulaku.com>
2020-10-16 20:07:39 +0800

    修改deploy

--- deploy.py
+++ deploy.py
@@ -1,8 +1,5 @@
 import os
 commit_log = input("输入提交日志：")
-
-
-print('git commit -am "%s"' % commit_log)
 os.system('git commit -am "%s"' % commit_log)
 os.system("git push")
 os.system("mkdocs gh-deploy --force")
